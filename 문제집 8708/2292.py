5bd8a38a5b6460d8086e69d1d7a7989f22ba8b68
Minseok Song <100372598+M1nseokSong@users.noreply.github.com>
2023-05-04 22:51:10 +0900

    브론즈2 벌집 코드리뷰 완료

--- 문제집 8708/2292.py
+++ 문제집 8708/2292.py
@@ -12,3 +12,17 @@ def acc(n):
         level += 6*x 
         x+=1
 print(acc(n))
+
+# 수정본 ( level + 6*x)
+n = int(input())
+def acc(n):
+    if n == 1:
+        return 1
+    level = 2
+    x = 1
+    while(True):
+        level += 6*x 
+        if n < level:
+            return x+1
+        x+=1
+print(acc(n))
