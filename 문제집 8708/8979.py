d9a09fe13c845021b4fd1e6ec5b7858bac0df336
Minseok Song <100372598+M1nseokSong@users.noreply.github.com>
2023-05-09 00:02:19 +0900

    실버5 올림픽 코드리뷰 완료

--- 문제집 8708/8979.py
+++ 문제집 8708/8979.py
@@ -12,6 +12,21 @@ medal.sort(reverse=True, key=lambda x: x[1])
 index = [medal[i][0] for i in range(n)].index(k)
 result = [medal[i][1] for i in range(n)].index(medal[index][1])
 print(result+1)
+
+#--------------------------------------------------------#
+# 고친 풀이 
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+medal = [0] * n
+gold, silver, bronze = 1000000, 1, 0.000001
+for _ in range(n):
+    t, g, s, b = map(int, input().split())
+    medal[t-1] = g*gold + s*silver + b*bronze
+weight = medal[k-1]
+medal.sort(reverse=True)
+result = medal.index(weight)
+print(result+1)
 #--------------------------------------------------------#
 # N, K = map(int, input().split())
 
