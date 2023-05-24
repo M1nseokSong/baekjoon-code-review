9c656ffbe021bebb4d98d2bbae40a2fe260e6306
Minseok Song <100372598+M1nseokSong@users.noreply.github.com>
2023-05-24 21:38:17 +0900

    실버3 햄거버 분배 코드리뷰 완료

--- 문제집 8708/19941.py
+++ 문제집 8708/19941.py
@@ -13,3 +13,22 @@ for i, ham in enumerate(burger):
                 result += 1
                 break
 print(result)
+
+-------- 팀원 풀이 (사람 중심이 아니라 햄버거, 사람을 쌍으로 봄)
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+PH = list(input()) # Person or Hamburger
+
+ans = 0
+for i in range(len(PH)):
+    if PH[i] == 'X': # 먹은 햄버거와 먹은 사람에는 임의로 'X'를 할당한다.
+        continue
+    
+    for j in range(i+1, min(i+K+1, len(PH))):
+        if (PH[i] == 'P' and PH[j] == 'H') or (PH[i] == 'H' and PH[j] == 'P'):
+            PH[i] = PH[j] = 'X'
+            ans += 1
+            break
+
+print(ans)
