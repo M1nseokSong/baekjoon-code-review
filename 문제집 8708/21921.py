a405657059abb60695dd069e20f5cf7ef1e9d85b
Minseok Song <100372598+M1nseokSong@users.noreply.github.com>
2023-05-24 22:20:50 +0900

    실버3 블로그 코드리뷰 완료

--- 문제집 8708/21921.py
+++ 문제집 8708/21921.py
@@ -25,3 +25,27 @@ def blog():
     return 
 
 blog()
+#--------------------------------팀원 풀이 --------------------------------------------#
+import sys
+input = sys.stdin.readline
+N, X = map(int, input().split())
+num_visitors = list(map(int, input().split()))
+
+def find_max_visitor():
+    curr_sum = sum(num_visitors[:X])
+    max_sum = curr_sum
+    cnt = 1
+    for i in range(1, N-X+1):
+        curr_sum = curr_sum - num_visitors[i-1] + num_visitors[i+X-1]
+        if curr_sum == max_sum:
+            cnt += 1
+        elif curr_sum > max_sum:
+            cnt = 1
+            max_sum = curr_sum
+
+    return max_sum, cnt
+    
+num, cnt = find_max_visitor()
+print("SAD") if num == 0 else print(f"{num}\n{cnt}")
+
+#최대값이랑 개수 찾는 문제는 내 방식보다 팀원의 방식이 좋아보임. 속도 측면에서
